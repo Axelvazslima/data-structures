b717faecf708b2e33753acfd2581a5021d0565a2
Áxel Vaz <106829115+Axelvazslima@users.noreply.github.com>
2023-11-15 13:47:19 -0300

    :zap: Updates

---
+++ fundamentals/Recursion.java
@@ -0,0 +1,32 @@
+package fundamentals;
+
+// Recursion Example.
+// Major topic when it comes to algorithms.
+// Loop logic without using it.
+// If you don't have a base case, it's going to loop forever. And if you get a single detail wrong, the same exact thing is probably going to happen, so you must really know what you are doing, and for that, you must be consistent in practicing to get confident with it.
+
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println("This code is an example of recursion.\nIt doesn't use a traditional loop whatsoever, but it works just like one.");
+
+        int counter = 0;
+
+        // Starting from 0
+        Recursion.recursionFundamentals(counter);
+        System.out.println("\n------------------------------\n");
+
+        // Starting from 1
+        Recursion.recursionFundamentals(++counter);
+    }
+
+    private static void recursionFundamentals(int counter){
+
+        // Base Case
+        // O(1);
+
+        if (counter <= 10) {
+            System.out.println(counter);
+            Recursion.recursionFundamentals(counter + 1);
+        }
+    }
+}
