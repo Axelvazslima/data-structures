14db924d50b88d77cdac434b845ab5f07c64f8ba
Áxel Vaz <106829115+Axelvazslima@users.noreply.github.com>
2023-12-04 12:39:12 -0300

    :art: Cleaner code and better implementations

--- algorithms/sorting/selectionsort/README.md
+++ algorithms/sorting/selectionsort/README.md
@@ -2,6 +2,6 @@
 
 Selection Sort is used to order a collection in O(N²) time complexity.
 
-Here, you will find various implementations of the Selection Sort using recursion, for loops, and while loops.
+Here, you will find various implementations of the Selection Sort Algorithm.
 
 You can learn more about it [here](https://axelvaz.notion.site/Selection-Sort-7922fb8534b24766ad9529b5c7508f14?pvs=4)
--- algorithms/sorting/selectionsort/SelectionSort.java
+++ algorithms/sorting/selectionsort/SelectionSort.java
@@ -1,78 +1,50 @@
 package algorithms.sorting.selectionsort;
 
 import java.util.Arrays;
+import algorithms.PiecesOfCodeIMayNeed;
 
 // N² time complexity
 // Selects the lowest element (minimum) and insert it into the first spot
 public class SelectionSort {
-    private static int[] nums = {2, 1, 2, 4, 1, 5, 10, 2, 7, 5, 6, 9, 23, 20, 24, 627};
+    private static int[] nums = PiecesOfCodeIMayNeed.nums;
 
     public static void main(String[] args) {
         System.out.println("The list is " + Arrays.toString(nums) + "\n");
         forImplementationSelectionSort(nums);
         whileImplementationSelectionSort(nums);
-        recursionImplementationSelectionSort(nums, 0);
+        System.out.println("Notice that it didn't change the original list (because I did not want to):\n" + Arrays.toString(nums) + "\n");
     }
 
     private static void forImplementationSelectionSort(int[] nums){
-        for (int i = 0; i < nums.length; i++) {
+        int[] temp = nums.clone();
+        for (int i = 0; i < temp.length; i++) {
             int minIndex = i;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[minIndex] > nums[j]) {
+            for (int j = i + 1; j < temp.length; j++) {
+                if (temp[minIndex] > temp[j]) {
                     minIndex = j;
                 }
             }
-            swap(nums, i, minIndex);
+            PiecesOfCodeIMayNeed.swap(temp, i, minIndex);
         }
-        System.out.println("The For Loop implemenation:\n" + Arrays.toString(nums) + "\n");
+        System.out.println("The For Loop implemenation:\n" + Arrays.toString(temp) + "\n");
     }
 
     private static void whileImplementationSelectionSort(int[] nums){
+        int[] temp = nums.clone();
+
         int i = 0;
-        while (i < nums.length) {
+        while (i < temp.length) {
             int j = i + 1;
             int minIndex = i;
-            while (j < nums.length) {
-                if (nums[minIndex] > nums[j]) {
+            while (j < temp.length) {
+                if (temp[minIndex] > temp[j]) {
                     minIndex = j;
                 }
                 j++;
             }
-            swap(nums, i, minIndex);
+            PiecesOfCodeIMayNeed.swap(temp, i, minIndex);
             i++;
         }
-        System.out.println("\nThe While Loop implementation:\n" + Arrays.toString(nums) + "\n");
-    }
-
-    // Outer step of the recursive solution for the Selection Sort Algorithm
-    private static void recursionImplementationSelectionSort(int[] nums, int position){
-        if (position >= nums.length - 1) {
-            System.out.println("\nRecursive approach:\n" + Arrays.toString(nums) + "\n");
-            return;
-        }
-        swap(nums, position, innerStepRecursion(nums, position));
-        position += 1;
-        recursionImplementationSelectionSort(nums, position);
-    }
-
-    // Inner step of the Selection Sort Algorithm
-    private static int innerStepRecursion(int[] nums, int position){
-        int minIndex = position;
-        if (position < nums.length - 1) {
-            if (nums[minIndex] > nums[position]) {
-                minIndex = position;
-                position += 1;
-                return innerStepRecursion(nums, position);
-            }
-        }
-        return minIndex;
-    }
-
-    // My implementation of the swap method to change where the elemnts are placed in the collection (Array, in this case)
-    private static int[] swap(int[] nums, int index, int indexMin){
-        int a = nums[index];
-        nums[index] = nums[indexMin];
-        nums[indexMin] = a;
-        return nums;
+        System.out.println("\nThe While Loop implementation:\n" + Arrays.toString(temp) + "\n");
     }
 }
