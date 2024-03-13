92ca976689897d6a9201a2d5e9dd1a1a96b5ecb4
Axelvazslima <axel.vaz.souto.lima@ccc.ufcg.edu.br>
2024-03-13 00:16:10 -0300

    Insertion Sort

--- algorithms/sorting/insertionsort/InsertionSort.java
+++ algorithms/sorting/insertionsort/InsertionSort.java
@@ -1,34 +1,46 @@
-package algorithms.sorting.insertionsort;
-
 import java.util.Arrays;
 
-import algorithms.PiecesOfCodeIMayNeed;
-
 public class InsertionSort {
-    private static int[] nums = PiecesOfCodeIMayNeed.nums.clone();
 
     public static void main(String[] args) {
-        PiecesOfCodeIMayNeed.welcomeMessage();
+        int[] seq = new int[]{4, 3, 2, 5, 6, 7, 8, 100, 1000, 0, -2, 3, 22, -4, 9, 10};
+        System.out.println("The original Array is: " + Arrays.toString(seq));
+        forLoopImplementationInsertionSort(seq);
 
-        forLoopImplementationInsertionSort(nums);
+        System.out.println("\nThe original Array is: " + Arrays.toString(seq) + "\nAs you can see, it didn't change at all.\n");
 
-        PiecesOfCodeIMayNeed.goodbyeMessage();
+        insertionSort(seq, 1, 1);
+        System.out.println("After running the Recursive Insertion Sort... This one changes the Original Array:");
+        System.out.println(Arrays.toString(seq));
+
     }
 
     private static void forLoopImplementationInsertionSort(int[] nums){
-        for (int i = 1; i < nums.length; i++) {
+        int[] temp = nums.clone();
+        for (int i = 1; i < temp.length; i++) {
 
             for (int j = i; j > 0; j--) {
 
                 // Modifying the sign from > to < orders it in reverse
-                if (nums[j] > nums[j - 1]) {
+                if (temp[j] > temp[j - 1]) {
                     break;
                 }
 
-                PiecesOfCodeIMayNeed.swap(nums, j, j - 1);
+                PiecesOfCodeIMayNeed.swap(temp, j, j - 1);
             }
         }
 
-        System.out.println("\nFor Loop Implementation: " + Arrays.toString(nums) + "\n");
+        System.out.println("\nFor Loop Implementation: " + Arrays.toString(temp) + "\nThis method doesn't change the original array.\n");
+    }
+
+    private static void insertionSort(int[] seq, int idx, int innerIdx) {
+        if (idx >= seq.length) return;
+        if (innerIdx > 0) {
+            if (seq[innerIdx] <= seq[innerIdx - 1]) {
+                PiecesOfCodeIMayNeed.swap(seq, innerIdx, innerIdx - 1);
+            }
+        }
+        idx++;
+        insertionSort(seq, idx, idx);
     }
 }
